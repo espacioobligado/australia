90e00a9bedb0051861c1b1c75d05057f5fd4291b
pola23423 <vzbekea068@tormails.com>
2024-06-25 12:10:50 -0300

    Mensaje de confirmación

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { isServer }) {
-      config.module.rules.push({
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/media/[name].[hash][ext]',
-        },
-      });
+    // webpack(config, { isServer }) {
+    //   config.module.rules.push({
+    //     test: /\.(ogg|mp3|wav|mpe?g)$/i,
+    //     type: 'asset/resource',
+    //     generator: {
+    //       filename: 'static/media/[name].[hash][ext]',
+    //     },
+    //   });
   
-      return config;
-    },
+    //   return config;
+    // },
   };
   
   export default nextConfig;
