ef42765419356338c0fc34200be941ba78225aab
pola23423 <vzbekea068@tormails.com>
2024-06-28 19:23:48 -0300

    Mensaje de confirmación

--- app/components/socket/socket.js
+++ app/components/socket/socket.js
@@ -3,7 +3,7 @@
 import { io } from "socket.io-client";
 
 // const URL = "http://localhost:3000";
-const URL = "https://quienviene.vercel.app:3000";
+const URL = "https://quienviene.vercel.app:5000";
 
 
 export const socket = io(URL, { autoConnect: false },{
