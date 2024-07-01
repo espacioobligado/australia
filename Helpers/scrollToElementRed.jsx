6eca5ca9f4a783578a7574d472c066ff1410b76d
pola23423 <vzbekea068@tormails.com>
2024-07-01 16:29:29 -0300

    Mensaje de confirmación

--- Helpers/scrollToElementRed.jsx
+++ Helpers/scrollToElementRed.jsx
@@ -1,39 +1,42 @@
 import { toast } from 'react-toastify';
 
-export const scrollToElementRed = (elements, lastValue ) => {
-// ,usuario
+export const scrollToElementRed = (elements, lastValue) => {
     const scrollToDiv = (id) => {
-                const element = document.getElementById(`${id}`);
+        const element = document.getElementById(`${id}`);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+            element.classList.add('blinkRed');
+        
+            setTimeout(() => {
+                element.classList.remove('blinkRed');
+            }, 5000);
 
-                if (element) {
-                    element.scrollIntoView({ behavior: 'smooth' });
-                    element.classList.add('blinkRed');
-                
-                    setTimeout(() => {
-                        element.classList.remove('blinkRed');  
-                    }, 5000);
-                    // toast.error(`Tu usuario es ${usuario} y creaste esta publicación!`, {
-                    //     position: "top-center",
-                    //     autoClose: false,
-                    //     hideProgressBar: false,
-                    //     closeOnClick: true,
-                    //     draggable: true,
-                    //     progress: undefined,
-                    //     theme: "light",
-                    //     style:{marginTop:'5px'}
-                    // });
-                }  
+            // Descomentar si necesitas mostrar un toast
+            // toast.error(`Tu usuario es ${usuario} y creaste esta publicación!`, {
+            //     position: "top-center",
+            //     autoClose: false,
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     draggable: true,
+            //     progress: undefined,
+            //     theme: "light",
+            //     style:{marginTop:'5px'}
+            // });
+        }
     };
 
-    elements.some((objeto, index) => {
+    const elementFound = elements.some((objeto, index) => {
         if (objeto.id === lastValue) {
             scrollToDiv(index);
-        } else{
-            setTimeout(() => {
-                window.location.reload()      
-            }, 1000);
-            console.log('reload') 
+            return true; // Detener la iteración al encontrar el elemento
         }
-        return false;  
+        return false; // Continuar iterando
     });
-}
+
+    // Si no se encuentra el elemento, recargar la página después de 1 segundo
+    if (!elementFound) {
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+    }
+};
