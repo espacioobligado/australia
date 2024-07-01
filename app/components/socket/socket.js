a08d958bf531a36e2e8a78579570e6b2003a24ce
pola23423 <vzbekea068@tormails.com>
2024-07-01 15:53:13 -0300

    Mensaje de confirmación

--- app/components/socket/socket.js
+++ app/components/socket/socket.js
@@ -1,34 +1,34 @@
 "use client";
 
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
-// // const URL = "http://localhost:3000";
-// const URL = "https://quienviene.vercel.app";
-
-
-// export const socket = io(URL, {
-//     autoConnect: false, // Conexión manual
-//     auth: {
-//       username: '' // Asegúrate de pasar el nombre de usuario si es necesario
-//     }
-//   });
+// const URL = "http://localhost:3000";
+const URL = "https://quienviene.vercel.app";
 
 
+export const socket = io(URL, {
+    autoConnect: false, // Conexión manual
+    auth: {
+      username: '' // Asegúrate de pasar el nombre de usuario si es necesario
+    }
+  });
 
 
-import * as Ably from 'ably';
-import { AblyProvider, useChannel, usePresence } from 'ably/react';
 
 
-const client = {
-  key: 'XAw4Vw.IHfvyQ:5pRNv2ZMguWRaVatjRo9I3F0A3m0m-uhPGrLULQYGKI',
-  clientId: 'your-ably-client-id',
-}
+// import * as Ably from 'ably';
+// import { AblyProvider, useChannel, usePresence } from 'ably/react';
 
 
-root.render(
-  <AblyProvider client={client}>
-    <App />
-  </AblyProvider>
-)
+// const client = {
+//   key: 'XAw4Vw.IHfvyQ:5pRNv2ZMguWRaVatjRo9I3F0A3m0m-uhPGrLULQYGKI',
+//   clientId: 'your-ably-client-id',
+// }
+
+
+// root.render(
+//   <AblyProvider client={client}>
+//     <App />
+//   </AblyProvider>
+// )
 
